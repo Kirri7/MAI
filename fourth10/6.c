23ceb8c4110ec38a214f47b2b48dd492d79ef9eb
Kirri7 <->
2023-11-25 21:14:13 +0300

    add int stack + shuntingYard

--- fourth10/6.c
+++ fourth10/6.c
@@ -34,6 +34,212 @@ static const char* errorMessages[] = {
 
 #define BUFFER_SIZE 100
 
+typedef struct SNode {
+    int data;
+    struct SNode* next;
+} SNode;
+
+typedef struct Stack {
+    SNode* top;
+} Stack;
+
+SNode* createSNode(const int data) {
+    SNode* newNode = (SNode*)malloc(sizeof(SNode));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void initStack(Stack* stack) {
+    stack->top = NULL;
+}
+
+int isEmpty(Stack* stack) {
+    return (stack->top == NULL);
+}
+
+void push(Stack* stack, const int data) {
+    SNode* newNode = createSNode(data);
+    newNode->next = stack->top;
+    stack->top = newNode;
+}
+
+int pop(Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Стек пуст 🪣\n");
+        return -1;
+    }
+    int poppedData = stack->top->data;
+    SNode* temp = stack->top;
+    stack->top = stack->top->next;
+    free(temp);
+    return poppedData;
+}
+
+int peek(Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Стек пуст 🪣\n");
+        return -1;
+    }
+    return stack->top->data;
+}
+
+void deleteStack(Stack* stack) {
+    while (stack->top != NULL) {
+        SNode* temp = stack->top;
+        stack->top = stack->top->next;
+        free(temp);
+    }
+}
+
+int isoperator(const char chr) {
+    // todo
+    switch (chr) {
+        default:
+            return 0;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+        case '^':
+            return 1;
+    }
+}
+
+int opPr(const char chr) {
+    // todo
+    switch(chr) {
+        default:
+            return -1;
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+        case '%':
+            return 2;
+        case '^':
+            return 3;
+    }
+}
+
+ErrorCode checkOutputOverflow(const int i, char output[BUFFER_SIZE]) {
+    if (i >= BUFFER_SIZE-1) {
+        output[BUFFER_SIZE-1] = '\0';
+        return OVERFLOW_ERROR;
+    }
+    return SUCCESS;
+}
+
+ErrorCode shuntingYard(const char input[], int inpLen, char output[BUFFER_SIZE]) {
+    Stack stack;
+    initStack(&stack);
+    memset(output, '\0', BUFFER_SIZE);
+    int j = 0;
+
+    for (int i = 0; i < inpLen; ++i) {
+        if (checkOutputOverflow(j, output) != SUCCESS) {
+            deleteStack(&stack);
+            return OVERFLOW_ERROR;
+        }
+        char chr = input[i];
+
+        if (isdigit(chr)) 
+        {
+            output[j++] = chr;
+            while (isdigit(input[++i])) {
+                output[j++] = input[i];
+            }
+            output[j++] = ' ';
+            --i;
+            continue;
+        } 
+        else if (isoperator(chr)) 
+        {
+            char opr1 = chr;
+            char opr2 = ' ';
+            if (!isEmpty(&stack))
+                opr2 = peek(&stack);
+            if (opr1 != '^') 
+            {   // правоассоциативность
+                while (!isEmpty(&stack) && opPr(opr1) <= opPr(opr2) && isoperator(opr2)) 
+                {
+                    opr2 = pop(&stack);
+                    output[j++] = opr2;
+                    output[j++] = ' ';
+                    if (checkOutputOverflow(j, output) != SUCCESS) {
+                        deleteStack(&stack);
+                        return OVERFLOW_ERROR;
+                    }
+                    if (!isEmpty(&stack)) {
+                        opr2 = peek(&stack);
+                    } else {
+                        break;
+                    }
+                }
+                push(&stack, opr1);
+            }
+            else 
+            {   // левоассоциативность
+                while (!isEmpty(&stack) &&opPr(opr1) < opPr(opr2) && isoperator(opr2))
+                {
+                    opr2 = pop(&stack);
+                    output[j++] = opr2;
+                    output[j++] = ' ';
+                    if (checkOutputOverflow(j, output) != SUCCESS) {
+                        deleteStack(&stack);
+                        return OVERFLOW_ERROR;
+                    }
+                    if (!isEmpty(&stack)) {
+                        opr2 = peek(&stack);
+                    } else {
+                        break;
+                    }
+                }
+                push(&stack, opr1);
+            }
+        }
+        else if (chr == '(')
+        {
+            push(&stack, '(');
+        }
+        else if (chr == ')')
+        {
+            while (!isEmpty(&stack) && peek(&stack) != '(') {
+                output[j++] = pop(&stack);
+                output[j++] = ' ';
+            }
+            if (isEmpty(&stack)) {
+                deleteStack(&stack);
+                return OPEN_BRACE_MISSING;
+            } else {
+                pop(&stack);
+            }
+        }
+        else if (!isspace(chr) && chr != '\0' && chr != '\n')
+        {
+            deleteStack(&stack);
+            return UNKNOWN_SYMBOL;
+        }
+    }
+    while (!isEmpty(&stack))
+    {
+        if (checkOutputOverflow(j, output) != SUCCESS) {
+            deleteStack(&stack);
+            return OVERFLOW_ERROR;
+        }
+        if (peek(&stack) == '(') {
+            deleteStack(&stack);
+            return CLOSE_BRACE_MISSING;
+        }
+
+        output[j++] = pop(&stack);
+        output[j++] = ' ';
+    }
+    return SUCCESS;
+}
+
 struct Node {
     int data;
     int isNumber;
@@ -72,20 +278,6 @@ void inorderTraversal(struct Node* root) {
 ErrorCode atExit(ErrorCode code, struct Node* root) {
     destroyTree(root);
     return code;
-}
-
-int isoperator(const char chr) {
-    switch (chr) {
-        default:
-            return 0;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '%':
-        case '^':
-            return 1;
-    }
 }
 
 ErrorCode convertToExpressionTree(char* expression, struct Node** result) {
