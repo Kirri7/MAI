bc36039479762faf999311293962b06c0da9f923
Kirri7 <86826650+Kirri7@users.noreply.github.com>
2023-12-27 10:04:14 +0300

    add completed task5

--- third10/5.c
+++ third10/5.c
@@ -61,12 +61,28 @@ int compare(const void *x, const void *y) {
     return strcmp(S1.group, S2.group);
 }
 
-int cmpA(const void *x, const void *y) {
-    return compare(x, y);
+int cmpID(const void *x, const void *y) {
+    Student S1 = *(Student*)x;
+    Student S2 = *(Student*)y;
+    return S1.id - S2.id;
 }
 
-int cmpD(const void *x, const void *y) {
-    return compare(y, x); // или -1 * compare
+int cmpLastName(const void *x, const void *y) {
+    Student S1 = *(Student*)x;
+    Student S2 = *(Student*)y;
+    return strcmp(S1.lastName, S2.lastName);
+}
+
+int cmpName(const void *x, const void *y) {
+    Student S1 = *(Student*)x;
+    Student S2 = *(Student*)y;
+    return strcmp(S1.firstName, S2.firstName);
+}
+
+int cmpGroup(const void *x, const void *y) {
+    Student S1 = *(Student*)x;
+    Student S2 = *(Student*)y;
+    return strcmp(S1.group, S2.group);
 }
 
 int validateMarks(unsigned char arr[5]) {
@@ -99,6 +115,39 @@ void putData(FILE *fileOut, Student *students, int studCnt) {
             student.marks[3],
             student.marks[4]);
     }
+    fflush(fileOut);
+}
+
+void putIdData(FILE *fileOut, Student *students, int studCnt) {
+    if (!fileOut || !students || 0 >= studCnt)
+        return;
+
+    for (int i = 0; i < studCnt; ++i) {
+        Student student = *(students+i);
+        if (!student.marks)
+            return;
+        double localAverage = 0;
+        for (int j = 0; j < 5; ++j) 
+            localAverage += student.marks[j]-'0';
+        localAverage /= 5;
+
+        // fprintf(fileOut, "%u %s %s %s %c %c %c %c %c\n", 
+        //     student.id, 
+        //     student.firstName, 
+        //     student.lastName, 
+        //     student.group,
+        //     student.marks[0],
+        //     student.marks[1],
+        //     student.marks[2],
+        //     student.marks[3],
+        //     student.marks[4]);
+        fprintf(fileOut, "%s %s %s %f\n", 
+            student.firstName, 
+            student.lastName, 
+            student.group,
+            localAverage);
+    }
+    fflush(fileOut);
 }
 
 void putHigherThanAverageData(FILE *fileOut, Student *students, int studCnt) {
@@ -130,17 +179,22 @@ void putHigherThanAverageData(FILE *fileOut, Student *students, int studCnt) {
 
         if (localAverage <= average)
             continue;
-        fprintf(fileOut, "%u %s %s %s %c %c %c %c %c\n", 
-            student.id, 
+        // fprintf(fileOut, "%u %s %s %s %c %c %c %c %c\n", 
+        //     student.id, 
+        //     student.firstName, 
+        //     student.lastName, 
+        //     student.group,
+        //     student.marks[0],
+        //     student.marks[1],
+        //     student.marks[2],
+        //     student.marks[3],
+        //     student.marks[4]);
+        fprintf(fileOut, "%s %s\n",
             student.firstName, 
-            student.lastName, 
-            student.group,
-            student.marks[0],
-            student.marks[1],
-            student.marks[2],
-            student.marks[3],
-            student.marks[4]);
+            student.lastName);
+        
     }
+    fflush(fileOut);
 }
 
 ErrorCode searchData(char search[], char mode, Student *database, int dataSize, Student **answ, int *answSize) {
@@ -240,7 +294,7 @@ ErrorCode getData(FILE *fileIn, Student **database, int *dataSize) {
             return MALLOC_ERROR;
         }
         char empty;
-        if (sscanf(buffer, "%u %s %s %s %c %c %c %c %c %c", 
+        if (sscanf(buffer, "%u %49s %49s %49s %c %c %c %c %c %c", 
             &student.id, 
             student.firstName, 
             student.lastName,
@@ -277,28 +331,190 @@ ErrorCode getData(FILE *fileIn, Student **database, int *dataSize) {
     *dataSize = studCnt;
     return SUCCESS;
 }
+    
+int startCmdSession(Student *students, int studCnt, FILE *fileOut)
+{
+    /*
+    0 - ошибка
+    1 - успешное завершение
+    */
+    Student *answ;
+    int answSize;
+    while (1)
+    {
+        printf("> ");
+
+        char buffer[BUFFER_SIZE];
+        for (int i = 0; i < BUFFER_SIZE - 1; ++i) buffer[i] = '\0';
+        if (!fgets(buffer, sizeof(buffer), stdin)) 
+        {
+            printf("- переполнение буфера\n");
+            return 0;
+        }
+        // Очищаем буфер ввода
+        if (!(buffer[BUFFER_SIZE - 2] == '\0' || buffer[BUFFER_SIZE - 2] == '\n'))
+        {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF);
+        }
+
+        // для разделения Howmuch date -s нужен пробел
+        char* command = strtok(buffer, " \n");
+        if (command == NULL) 
+        {
+
+        }
+        else if (strcmp(command, "Logout") == 0)
+        {
+            printf("Выход...\n\n");
+            return 1;
+        }
+        else if (strcmp(command, "WriteBestStudents") == 0)
+        {
+            putHigherThanAverageData(fileOut, students, studCnt);
+        } 
+        else if (strcmp(command, "SortID") == 0)
+        {   
+            qsort(students, studCnt, sizeof(Student), cmpID);
+            putData(stdout, students, studCnt);
+        } 
+        else if (strcmp(command, "SortSurname") == 0)
+        {   
+            qsort(students, studCnt, sizeof(Student), cmpLastName);
+            putData(stdout, students, studCnt);
+        } 
+        else if (strcmp(command, "SortName") == 0)
+        {   
+            qsort(students, studCnt, sizeof(Student), cmpName);
+            putData(stdout, students, studCnt);
+        } 
+        else if (strcmp(command, "SortGroup") == 0)
+        {   
+            qsort(students, studCnt, sizeof(Student), cmpGroup);
+            putData(stdout, students, studCnt);
+        } 
+        else if (strcmp(command, "WriteIdStudent") == 0)
+        {   
+            // подходит для поискаID 37\n
+            char* argum = strtok(NULL, " \n");
+
+            if (argum == NULL) {
+                printf(" аргумент: <id>\n");
+                continue;
+            }
+
+            ErrorCode code = searchData(argum, 'i', students, studCnt, &answ, &answSize);
+            if (code != SUCCESS)
+                printf(" %s\n", errorMessages[code]);
+            else if (answSize == 0)
+                printf(" студент не найден\n");
+            else {
+                putIdData(fileOut, answ, answSize);
+                free(answ);
+            }
+        } 
+        else if (strcmp(command, "SearchID") == 0)
+        {   
+            char* argum = strtok(NULL, " \n");
+
+            if (argum == NULL) {
+                printf(" аргумент: <id>\n");
+                continue;
+            }
+
+            ErrorCode code = searchData(argum, 'i', students, studCnt, &answ, &answSize);
+            if (code != SUCCESS)
+                printf(" %s\n", errorMessages[code]);
+            else if (answSize == 0)
+                printf(" студент не найден\n");
+            else {
+                putData(stdout, answ, answSize);
+                free(answ);
+            }
+        } 
+        else if (strcmp(command, "SearchName") == 0)
+        {   
+            char* argum = strtok(NULL, " \n");
+
+            if (argum == NULL) {
+                printf(" аргумент: <first_name>\n");
+                continue;
+            }
+
+            ErrorCode code = searchData(argum, 'f', students, studCnt, &answ, &answSize);
+            if (code != SUCCESS)
+                printf(" %s\n", errorMessages[code]);
+            else if (answSize == 0)
+                printf(" студент не найден\n");
+            else {
+                putData(stdout, answ, answSize);
+                free(answ);
+            }
+        } 
+        else if (strcmp(command, "SearchSurname") == 0)
+        {   
+            char* argum = strtok(NULL, " \n");
+
+            if (argum == NULL) {
+                printf(" аргумент: <last_name>\n");
+                continue;
+            }
+
+            ErrorCode code = searchData(argum, 'l', students, studCnt, &answ, &answSize);
+            if (code != SUCCESS)
+                printf(" %s\n", errorMessages[code]);
+            else if (answSize == 0)
+                printf(" студент не найден\n");
+            else {
+                putData(stdout, answ, answSize);
+                free(answ);
+            }
+        } 
+        else if (strcmp(command, "SearchGroup") == 0)
+        {   
+            char* argum = strtok(NULL, " \n");
+
+            if (argum == NULL) {
+                printf(" аргумент: <group_name>\n");
+                continue;
+            }
+
+            ErrorCode code = searchData(argum, 'g', students, studCnt, &answ, &answSize);
+            if (code != SUCCESS)
+                printf(" %s\n", errorMessages[code]);
+            else if (answSize == 0)
+                printf(" группа не найдена\n");
+            else {
+                putData(stdout, answ, answSize);
+                free(answ);
+            }
+        } 
+        else if (strcmp(command, "Help") == 0)
+        {   
+            printf(" возможные команды: SearchGroup, SearchSurname, WriteBestStudents, SortName, SortID, Logout...\n");
+        } 
+        else 
+        {
+            printf(" ❗ команда не найдена\n");
+        }
+    }
+    printf("Выход...\n\n");
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("%s <входной файл> <флаг> <выходной файл>\n", argv[0]);
+    if (argc != 3) {
+        printf("%s <входной файл> <выходной файл>\n", argv[0]);
         return INCORRECT_INPUT;
     }
 
-    char *flagArg = argv[2];
-    if (flagArg[0] != '-' && flagArg[0] != '/') {
-        printf("%s\n", errorMessages[INCORRECT_INPUT]);
-        fflush(stdout);
-        return INCORRECT_INPUT;
-    }
-
-    ++flagArg;
-    if (strcmp(flagArg, "a") != 0 && strcmp(flagArg, "d") != 0) {
-        printf("Неподдерживаемый флаг или неверное количество параметров 🦉\n");
+    if (strcmp(argv[1], argv[2]) == 0) {
+        printf("<входной файл> совпадает с <выходной файл>\n");
         return INCORRECT_INPUT;
     }
 
     FILE *fileIn = fopen(argv[1], "r");
-    FILE *fileOut = fopen(argv[3], "w");
+    FILE *fileOut = fopen(argv[2], "w");
     if (fileIn == NULL || fileOut == NULL) {
         printf("%s\n", errorMessages[FILE_OPENING_ERROR]);
         fflush(stdout);
@@ -319,23 +535,23 @@ int main(int argc, char *argv[]) {
             return code;
     }
 
-    if (strcmp(flagArg, "a") == 0) {
-        qsort(students, studCnt, sizeof(Student), cmpA);
-    } else if (strcmp(flagArg, "d") == 0) {
-        qsort(students, studCnt, sizeof(Student), cmpD);
-    }
-
-    putData(fileOut, students, studCnt);
-    putHigherThanAverageData(stdout, students, studCnt);
+    printf("\nВведите команду или 'Help':\n");
+    fflush(stdout);
+    
+    int code1 = startCmdSession(students, studCnt, fileOut);
+    
+    // putData(fileOut, students, studCnt);
+    // putHigherThanAverageData(stdout, students, studCnt);
     // Student *answer;
     // int answSize;
     // searchData("PqusQwQO", 'g', students, studCnt, &answer, &answSize);
     // if (answSize != 0)
     //     putData(stdout, answer, answSize);
 
-    
-
     free(students);
     closeFiles(fileIn, fileOut);
-    return SUCCESS;
+    if (code1)
+        return SUCCESS;
+    else
+        return UNKNOWN_ERROR;
 }
